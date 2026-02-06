438d0c4c27ba461306d0a868371aa05136e565a2
Baldemar Estrada <Baldemar.estrada@fullman.cloud>
2026-02-06 19:18:50 +0000

    Initialize Spring Boot project structure

---
+++ src/main/java/com/coppel/coppel/CoppelApplication.java
@@ -0,0 +1,12 @@
+package com.coppel.coppel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public final class CoppelApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(CoppelApplication.class, args);
+    }
+}
